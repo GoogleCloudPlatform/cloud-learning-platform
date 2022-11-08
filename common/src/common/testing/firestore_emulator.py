aeaaecd4b2b55e9b02259c5eef0c97401ada358b
shubhammenroy <shubham.menroy@quantiphi.com>
2022-11-08 13:25:17 +0530

    Fix: Modified firestore_emulator.py so that this script can work on a Windows system.

--- common/src/common/testing/firestore_emulator.py
+++ common/src/common/testing/firestore_emulator.py
@@ -26,16 +26,22 @@ import pytest
 # pylint: disable=unused-argument,redefined-outer-name,unused-import
 from fastapi.testclient import TestClient
 from main import app
-
+import platform
 # recreate the emulator each module - could consider changing to session
 # pylint: disable = consider-using-with, subprocess-popen-preexec-fn
 @pytest.fixture
 def firestore_emulator():
 
-  emulator = subprocess.Popen(
-      "firebase emulators:start --only firestore --project fake-project",
-      shell=True,
-      preexec_fn=os.setsid)
+  winows = True if platform.system() == "Windows" else False
+  if winows:
+    emulator = subprocess.Popen(
+        "firebase emulators:start --only firestore --project fake-project",
+        shell=True)
+  else:
+    emulator = subprocess.Popen(
+        "firebase emulators:start --only firestore --project fake-project",
+        shell=True,
+        preexec_fn=os.setsid)
   time.sleep(15)
 
   os.environ["FIRESTORE_EMULATOR_HOST"] = "localhost:8080"
@@ -45,7 +51,11 @@ def firestore_emulator():
   # yield so emulator isn't recreated each test
   yield emulator
 
-  os.killpg(os.getpgid(emulator.pid), signal.SIGTERM)
+  if winows:
+    os.kill(emulator.pid, signal.CTRL_BREAK_EVENT)
+  else:
+    os.killpg(os.getpgid(emulator.pid), signal.SIGTERM)
+    
   # delete debug files
   # some get deleted, not all
 
