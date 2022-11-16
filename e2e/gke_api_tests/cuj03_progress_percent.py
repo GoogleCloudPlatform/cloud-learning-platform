02b24784f256dc0ec51f8ffb0edf11a10e908513
saurav <saurav.chakravarty@quantiphi.com>
2022-11-16 19:01:48 +0530

    added test users

--- e2e/gke_api_tests/cuj03_progress_percent.py
+++ e2e/gke_api_tests/cuj03_progress_percent.py
@@ -13,6 +13,6 @@ def test_get_progress_percentage():
   base_url = get_baseurl("lms")
   if not base_url:
     raise NotFoundErr("Unable to locate the service URL for lms")
-  res = requests.get(base_url + "/student/get_progress_percentage/?course_id=504551481098&student_email=saurav.minimi@gmail.com")
+  res = requests.get(base_url + "/student/get_progress_percentage/?course_id=504551481098&student_email=test_user_1@dhodun.altostrat.com")
   result = res.json()
   assert res.status_code == 200
--- microservices/lms/src/routes/student_test.py
+++ microservices/lms/src/routes/student_test.py
@@ -24,7 +24,7 @@ SUCCESS_RESPONSE = {"status": "Success"}
 
 
 def test_get_student_course_progress_percent(client_with_emulator):
-  url = API_URL + "/student/get_progress_percentage/?course_id=504551481098&student_email=saurav.minimi@gmail.com"
+  url = API_URL + "/student/get_progress_percentage/?course_id=504551481098&student_email=test_user_1@dhodun.altostrat.com"
   with mock.patch("routes.student.classroom_crud.get_course_work_list", return_value = [{},{}]):
       with mock.patch("routes.student.classroom_crud.get_submitted_course_work_list", return_value = [{"state":"TURNED_IN"},{"state":"TURNED_IN"}]):
         resp = client_with_emulator.get(url)
