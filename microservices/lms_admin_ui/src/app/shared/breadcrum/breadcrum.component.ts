402dbc47d9c6b64f142473f9e26b503513539178
saurav <saurav.chakravarty@quantiphi.com>
2022-11-30 14:51:14 +0530

    added missing breadcrum component

---
+++ microservices/lms_admin_ui/src/app/shared/breadcrum/breadcrum.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
+
+@Component({
+  selector: 'app-breadcrum',
+  templateUrl: './breadcrum.component.html',
+  styleUrls: ['./breadcrum.component.scss']
+})
+export class BreadcrumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
