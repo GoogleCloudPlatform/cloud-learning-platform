a7c7ea43b1bdfa41f8c77a46442b6477496c2b22
shubhammenroy <shubham.menroy@quantiphi.com>
2022-12-02 18:04:45 +0530

    fix e2e test

--- microservices/lms/src/utils/helper.py
+++ microservices/lms/src/utils/helper.py
@@ -14,8 +14,8 @@ def get_gke_pd_sa_key_from_secret_manager():
 
   client = secretmanager.SecretManagerServiceClient()
   secret_id = "gke-pod-sa-key"
-  secret_name = f"projects/core-learning-services-dev\
-  /secrets/{secret_id}/versions/latest"
+  secret_name = ("projects/core-learning-services-dev/" +
+                 f"secrets/{secret_id}/versions/latest")
 
   response = client.access_secret_version(request={"name": secret_name})
   crc32c = google_crc32c.Checksum()
