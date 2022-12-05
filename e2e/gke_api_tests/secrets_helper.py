cba15651e4ba3b01c4e2a01a8526a92a8c695c0d
shubhammenroy <shubham.menroy@quantiphi.com>
2022-12-05 18:46:40 +0530

    fix: e2e test

--- e2e/gke_api_tests/secrets_helper.py
+++ e2e/gke_api_tests/secrets_helper.py
@@ -4,29 +4,33 @@ import os
 from google.cloud import secretmanager
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
-PROJECT_ID = os.getenv("PROJECT_ID","")
+
+PROJECT_ID = os.getenv("PROJECT_ID", "")
+
+
 def get_required_emails_from_secret_manager():
   """Get Project user emails for e2e
 
   Args:
   Returns:
     return the emails in dict format
-    """""
+    """ ""
 
   client = secretmanager.SecretManagerServiceClient()
-  test_user_secret_id="test-user-1-username"
-  lms_user_secret_id="lms-service-user"
+  test_user_secret_id = "test-user-1-username"
+  lms_user_secret_id = "lms-service-user"
   test_user_secret_name = f"projects/{PROJECT_ID}/secrets/{test_user_secret_id}/versions/latest"
   test_user_response = client.access_secret_version(
       request={"name": test_user_secret_name})
   lms_user_secret_name = f"projects/{PROJECT_ID}/secrets/{lms_user_secret_id}/versions/latest"
   lms_user_response = client.access_secret_version(
       request={"name": lms_user_secret_name})
-  data={
+  data = {
       "admin": lms_user_response.payload.data.decode("UTF-8"),
       "instructional_designer": test_user_response.payload.data.decode("UTF-8")
   }
   return data
+
 
 def get_student_email_and_token():
   """Get student email and token
@@ -36,21 +40,23 @@ def get_student_email_and_token():
     """
   client = secretmanager.SecretManagerServiceClient()
   student_email_secret_id = "personal-test-user-1-username"
-  student_token_secret_id="add_student_token"
+  student_token_secret_id = "add_student_token"
   student_email_name = f"projects/{PROJECT_ID}/secrets/{student_email_secret_id}/versions/latest"
   student_token_name = f"projects/{PROJECT_ID}/secrets/{student_token_secret_id}/versions/latest"
   student_email_response = client.access_secret_version(
       request={"name": student_email_name})
   student_token_response = client.access_secret_version(
       request={"name": student_token_name})
-  credentials_dict=json.loads(student_token_response.payload.data.decode("UTF-8"))
-  creds = Credentials.from_authorized_user_info(credentials_dict,scopes=credentials_dict["scopes"])
+  credentials_dict = json.loads(
+      student_token_response.payload.data.decode("UTF-8"))
+  creds = Credentials.from_authorized_user_info(
+      credentials_dict, scopes=credentials_dict["scopes"])
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
-      credentials_dict=creds.to_json()
+      credentials_dict = creds.to_json()
   data = {
       "email": student_email_response.payload.data.decode("UTF-8"),
-      "credentials": 
+      "credentials": credentials_dict
   }
   return data
