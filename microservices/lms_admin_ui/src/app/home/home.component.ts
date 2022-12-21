44847e5a6224853e134d16896efbb2d1467ba21e
saurav <saurav.chakravarty@quantiphi.com>
2022-12-21 21:12:47 +0530

    resolve conflict

--- microservices/lms_admin_ui/src/app/home/home.component.ts
+++ microservices/lms_admin_ui/src/app/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
   searchCohortTerm: string
   searchCourseTemplate: string
   constructor(public dialog: MatDialog, public _HomeService: HomeService) {
-    console.log('env var', environment.apiurl);
+    // console.log('env var', environment.apiurl);
   }
 
   ngOnInit(): void {
