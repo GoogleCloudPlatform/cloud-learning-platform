920e48c29e7b2f0321dc335a4b1b5749da832df1
pavansareddy-q <pavan.sareddy@quantiphi.com>
2023-02-06 19:15:10 +0530

    chore: updated assignment id with lti assignment id

--- microservices/classroom_shim/src/routes/launch.py
+++ microservices/classroom_shim/src/routes/launch.py
@@ -21,18 +21,18 @@ CLP_DOMAIN_URL = "https://core-learning-services-dev.cloudpssolutions.com"
 
 
 @router.get("/launch")
-def login(request: Request, assignment_id: Optional[str] = ""):
+def login(request: Request, lti_assignment_id: Optional[str] = ""):
   """This login endpoint will return the user to firebase login page
   Args:
-      assignment_id (str): unique id of the LTI Assignment
+      lti_assignment_id (str): unique id of the LTI Assignment
   Returns:
       Template response with the login page of the user
   """
   try:
     url = f"{CLP_DOMAIN_URL}/classroom-shim/api/v1/launch-assignment"
 
-    if assignment_id == "":
-      url = f"{url}?assignment_id={assignment_id}"
+    if lti_assignment_id:
+      url = f"{url}?lti_assignment_id={lti_assignment_id}"
 
     return templates.TemplateResponse("login.html", {
         "request": request,
@@ -49,12 +49,12 @@ def login(request: Request, assignment_id: Optional[str] = ""):
 
 @router.get("/launch-assignment")
 def launch_assignment(request: Request,
-                      assignment_id: Optional[str] = "",
+                      lti_assignment_id: Optional[str] = "",
                       user_details: dict = Depends(validate_token)):
   """This launch assignment will take the input assignment id and
   redirect the user to the appropriate LTI tool content item
   Args:
-      assignment_id (str): unique id of the LTI Assignment
+      lti_assignment_id (str): unique id of the LTI Assignment
   Returns:
       Redirects to the LTI tool launch url
   """
@@ -76,13 +76,13 @@ def launch_assignment(request: Request,
       return {"success": False, "message": "Internal server error"}
 
     user_id = user_data.get("user_id")
-    lti_assignment = LTIAssignment.find_by_id(assignment_id)
+    lti_assignment = LTIAssignment.find_by_id(lti_assignment_id)
     lti_content_item_id = lti_assignment.lti_content_item_id
 
     url = f"{CLP_DOMAIN_URL}/lti/api/v1/resource-launch-init?lti_content_item_id={lti_content_item_id}&user_id={user_id}"
     # TODO: verify assignment and user relationship
     # TODO: fetch data from request for custom param substitution
-    return RedirectResponse(url=url, headers={}, status_code=302)
+    return RedirectResponse(url=url, headers=headers, status_code=302)
 
   except ValidationError as e:
     Logger.error(e)
@@ -92,5 +92,4 @@ def launch_assignment(request: Request,
     raise ResourceNotFound(str(e)) from e
   except Exception as e:
     Logger.error(e)
-    raise e
     raise InternalServerError(str(e)) from e
