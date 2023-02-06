d801ff1a3956a68beb22104eade7ce6f65cea69e
pavansareddy-q <pavan.sareddy@quantiphi.com>
2023-02-06 18:58:11 +0530

    chore: fetched content item from lti assignment

--- microservices/classroom_shim/src/routes/launch.py
+++ microservices/classroom_shim/src/routes/launch.py
@@ -3,7 +3,12 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from config import ERROR_RESPONSES
+from common.models import LTIAssignment
 from common.utils.auth_service import validate_token
+from common.utils.errors import ResourceNotFoundException, ValidationError
+from common.utils.http_exceptions import (ResourceNotFound, InternalServerError,
+                                          BadRequest)
+from common.utils.logging_handler import Logger
 from typing import Optional
 import requests
 # pylint: disable=line-too-long
@@ -23,13 +28,23 @@ def login(request: Request, assignment_id: Optional[str] = ""):
   Returns:
       Template response with the login page of the user
   """
-  url = f"{CLP_DOMAIN_URL}/classroom-shim/api/v1/launch-assignment"
-  if assignment_id == "":
-    url = f"{url}?assignment_id={assignment_id}"
-  return templates.TemplateResponse("login.html", {
-      "request": request,
-      "redirect_url": url
-  })
+  try:
+    url = f"{CLP_DOMAIN_URL}/classroom-shim/api/v1/launch-assignment"
+
+    if assignment_id == "":
+      url = f"{url}?assignment_id={assignment_id}"
+
+    return templates.TemplateResponse("login.html", {
+        "request": request,
+        "redirect_url": url
+    })
+
+  except ValidationError as e:
+    Logger.error(e)
+    raise BadRequest(str(e)) from e
+  except Exception as e:
+    Logger.error(e)
+    raise InternalServerError(str(e)) from e
 
 
 @router.get("/launch-assignment")
@@ -43,30 +58,39 @@ def launch_assignment(request: Request,
   Returns:
       Redirects to the LTI tool launch url
   """
-  # verify user if it exists
-  user_email = user_details.get("email")
-  headers = {"Authorization": request.headers.get("Authorization")}
-  fetch_user_request = requests.get(
-      "http://user-management/user-management/api/v1/user/search",
-      params={"email": user_email},
-      headers=headers,
-      timeout=60)
+  try:
+    # verify user if it exists
+    user_email = user_details.get("email")
+    headers = {"Authorization": request.headers.get("Authorization")}
+    fetch_user_request = requests.get(
+        "http://user-management/user-management/api/v1/user/search",
+        params={"email": user_email},
+        headers=headers,
+        timeout=60)
 
-  if fetch_user_request.status_code == 200:
-    user_data = fetch_user_request.json().get("data")[0]
-  elif fetch_user_request.status_code == 404:
-    return {"success": False, "message": "User not found"}
-  elif fetch_user_request.status_code == 500:
-    return {"success": False, "message": "Internal server error"}
+    if fetch_user_request.status_code == 200:
+      user_data = fetch_user_request.json().get("data")[0]
+    elif fetch_user_request.status_code == 404:
+      return {"success": False, "message": "User not found"}
+    elif fetch_user_request.status_code == 500:
+      return {"success": False, "message": "Internal server error"}
 
-  user_id = user_data.get("user_id")
-  # TODO: change the content item logic to fetch data from assignments data model
-  if assignment_id != "":
-    lti_content_item_id = assignment_id
-  else:
-    lti_content_item_id = "Vwms1o5X2ibH0qdXgnPV"
+    user_id = user_data.get("user_id")
+    lti_assignment = LTIAssignment.find_by_id(assignment_id)
+    lti_content_item_id = lti_assignment.lti_content_item_id
 
-  url = f"{CLP_DOMAIN_URL}/lti/api/v1/resource-launch-init?lti_content_item_id={lti_content_item_id}&user_id={user_id}"
-  # TODO: verify assignment and user relationship
-  # TODO: fetch data from request for custom param substitution
-  return RedirectResponse(url=url, headers=headers, status_code=302)
+    url = f"{CLP_DOMAIN_URL}/lti/api/v1/resource-launch-init?lti_content_item_id={lti_content_item_id}&user_id={user_id}"
+    # TODO: verify assignment and user relationship
+    # TODO: fetch data from request for custom param substitution
+    return RedirectResponse(url=url, headers={}, status_code=302)
+
+  except ValidationError as e:
+    Logger.error(e)
+    raise BadRequest(str(e)) from e
+  except ResourceNotFoundException as e:
+    Logger.error(e)
+    raise ResourceNotFound(str(e)) from e
+  except Exception as e:
+    Logger.error(e)
+    raise e
+    raise InternalServerError(str(e)) from e
