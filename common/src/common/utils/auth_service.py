6b230ec2659d7d41e86d9e7d29b9c6173b5324ea
saurav <saurav.chakravarty@quantiphi.com>
2023-02-08 18:02:32 +0530

    added user type validation

--- common/src/common/utils/auth_service.py
+++ common/src/common/utils/auth_service.py
@@ -48,3 +48,45 @@ def validate_token(token: auth_scheme = Depends()):
     raise Unauthenticated(str(e)) from e
   except Exception as e:
     raise InternalServerError(str(e)) from e
+
+
+def validate_user_type_and_token(accepted_user_types:list,token: auth_scheme = Depends()):
+  """_summary_
+
+  Args:
+      token (auth_scheme, optional): _description_. Defaults to Depends().
+
+  Raises:
+      InvalidTokenError: _description_
+      Unauthenticated: _description_
+      InternalServerError: _description_
+
+  Returns:
+      Boolean: token is valid or not
+  """
+  try:
+    if token is None:
+      raise InvalidTokenError("Unauthorized")
+    token_dict = dict(token)
+    if token_dict["credentials"]:
+      api_endpoint = "http://authentication/authentication/api/v1/validate"
+      res = requests.get(
+          url=api_endpoint,
+          headers={
+              "Content-Type":
+              "application/json",
+              "Authorization":
+              f"{token_dict['scheme']} {token_dict['credentials']}"
+          },
+          timeout=60)
+      data = res.json()
+      if res.status_code == 200 and data["success"] is True and data["data"]["user_type"] in accepted_user_types:
+        return data.get("data")
+      else:
+        raise InvalidTokenError(data["message"])
+    else:
+      raise InvalidTokenError("Unauthorized")
+  except InvalidTokenError as e:
+    raise Unauthenticated(str(e)) from e
+  except Exception as e:
+    raise InternalServerError(str(e)) from e
