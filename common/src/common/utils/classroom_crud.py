4a7223f56f8159fe110da3c7081c296a1b6f01e3
Shruti Math <shruti.math@quantiphi.com>
2023-02-13 16:38:28 +0530

    commented first name ,last name in student profile

--- common/src/common/utils/classroom_crud.py
+++ common/src/common/utils/classroom_crud.py
@@ -409,8 +409,8 @@ def enroll_student(headers ,access_token, course_id,student_email,course_code):
   # last_name =profile["names"][0]["familyName"]
   # Call user API
   data = {
-  "first_name":profile["names"][0]["givenName"],
-  "last_name": profile["names"][0]["familyName"],
+  "first_name":"",
+  "last_name": "",
   "email":student_email,
   "user_type": "learner",
   "user_type_ref": "",
