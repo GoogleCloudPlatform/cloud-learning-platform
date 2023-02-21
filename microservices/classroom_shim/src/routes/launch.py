f58dc2a9e7db6a1b33320bbeec6c2a49db5bb55d
Ram Chaudhari <ram.chaudhari@quantiphi.com>
2023-02-21 14:29:39 +0530

    changes

--- microservices/classroom_shim/src/routes/launch.py
+++ microservices/classroom_shim/src/routes/launch.py
@@ -89,7 +89,7 @@ def launch_assignment(request: Request,
         timeout=60)
 
     if fetch_user_request.status_code == 200:
-      faculty_ser_data = fetch_user_request.json().get("data")[0]
+      user_data = fetch_user_request.json().get("data")[0]
     elif fetch_user_request.status_code == 404:
       raise UnauthorizedUserError("Unauthorized")
     else:
@@ -123,7 +123,17 @@ def launch_assignment(request: Request,
     user_type = user_details.get("user_type")
 
     if user_type == "learner":
-      pass
+      api_url = f"http://lms/lms/api/v1//sections/{lti_assignment.get('section_id')}/teachers/{user_email}"
+      fetch_user_mapping = requests.get(api_url, headers=headers, timeout=60)
+
+      if fetch_user_mapping.status_code == 200:
+        learner_data = fetch_user_mapping.json().get("data")
+      elif fetch_user_mapping.status_code == 404:
+        raise UnauthorizedUserError("Unauthorized")
+      else:
+        raise Exception(
+            "Internal server error from user mapping validation API")
+
     elif user_type == "faculty":
       api_url = f"http://lms/lms/api/v1//sections/{lti_assignment.get('section_id')}/teachers/{user_email}"
       fetch_user_mapping = requests.get(api_url, headers=headers, timeout=60)
