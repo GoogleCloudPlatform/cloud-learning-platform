427a1ee122d31a0e2f7e5bcbc60aa4d7dc22af9e
saurav <saurav.chakravarty@quantiphi.com>
2023-02-23 16:48:11 +0530

    test cases changes

--- microservices/lms/src/routes/student_test.py
+++ microservices/lms/src/routes/student_test.py
@@ -80,26 +80,29 @@ def create_fake_data():
 
 def test_get_progress_percentage(client_with_emulator):
   url = BASE_URL + "/sections/9xieFlma8bcWYyLPOg0c/" + \
-    "/get_progress_percentage/clplmstestuser1@gmail.com"
-
-  with mock.patch("routes.student.classroom_crud.get_course_work_list",
-                  return_value=[{}, {}]):
-    with mock.patch(
-        "routes.student.classroom_crud.get_submitted_course_work_list",
-        return_value=[{
-            "state": "TURNED_IN"
-        }, {
-            "state": "TURNED_IN"
-        }]):
-      resp = client_with_emulator.get(url)
+    "get_progress_percentage/clplmstestuser1@gmail.com"
+  with mock.patch\
+  ("routes.student.student_service.get_user_id",return_value="user_id"):
+    with mock.patch("routes.student.classroom_crud.get_course_work_list",
+                    return_value=[{}, {}]):
+      with mock.patch(
+          "routes.student.classroom_crud.get_submitted_course_work_list",
+          return_value=[{
+              "state": "TURNED_IN"
+          }, {
+              "state": "TURNED_IN"
+          }]):
+        resp = client_with_emulator.get(url)
   assert resp.status_code == 200
 
 def test_get_student_in_section(client_with_emulator):
   url = BASE_URL + "/sections/5/students/clplmstestuser1@gmail.com"
 
-  with mock.patch("routes.student.classroom_crud.if_user_exists_in_section",
-                  return_value=[{}, {}]):
-    resp = client_with_emulator.get(url)
+  with mock.patch\
+  ("routes.student.student_service.get_user_id",return_value="user_id"):
+    with mock.patch("routes.student.classroom_crud.if_user_exists_in_section",
+                    return_value={}):
+      resp = client_with_emulator.get(url)
   assert resp.status_code == 200
 
 def test_list_student_of_section(client_with_emulator):
