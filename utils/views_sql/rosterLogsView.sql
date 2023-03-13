0ffba1af52d59a018186ac07dfabbbbb974d01a9
shubhammenroy <shubham.menroy@quantiphi.com>
2023-03-13 16:34:00 +0530

    Modified roster view

--- utils/views_sql/rosterLogsView.sql
+++ utils/views_sql/rosterLogsView.sql
@@ -1,31 +1,28 @@
 CREATE OR REPLACE VIEW
   rosterLogsView AS
-SELECT * FROM
-(SELECT uuid	as userColl_uuid,	
-id as userColl_id,
-name as userColl_name,		
-emailAddress as userColl_emailAddress,
-photoUrl	as userColl_photoUrl,			
-verifiedTeacher	as userColl_verifiedTeacher,		
-permissions	as userColl_permissions,		
-event_type	as userColl_event_type,			
-timestamp	as userColl_timestamp,			
-message_id as userColl_message_id FROM(
+WITH user as (SELECT id,
+        name,		
+        emailAddress,
+        photoUrl,			
+        verifiedTeacher,		
+        permissions FROM(
 SELECT *, ROW_NUMBER() OVER(PARTITION BY id ORDER BY timestamp DESC) AS row_num
 FROM `lms_analytics.userCollection`
 ) a
     WHERE
       a.row_num = 1
-      AND a.event_type != 'DELETED') as user,(SELECT 
-      message_id as roster_message_id,
-        collection	as roster_collection,						
-        resource	as roster_resource,
-        publish_time	as roster_publish_time,			
-        event_type as roster_event_type,
-        `timestamp` as roster_timestamp FROM(
-SELECT *, ROW_NUMBER() OVER(PARTITION BY resource.userId ORDER BY timestamp DESC) AS row_num
-FROM `lms_analytics.rosterLogs`
-) b
-    WHERE
-      b.row_num = 1
-      AND b.event_type != 'DELETED') as roster where user.userColl_id=roster.roster_resource.userId
+      AND a.event_type != 'DELETED')
+SELECT 
+      roster.message_id as roster_message_id,
+        roster.collection	as roster_collection,					
+        roster.resource	as roster_resource,
+        roster.publish_time	as roster_publish_time,			
+        roster.event_type as roster_event_type,
+        roster.timestamp as roster_timestamp,
+        user.id as user_gaia_id,
+        user.name as user_name,		
+        user.emailAddress as user_emailAddress,
+        user.photoUrl	as user_photoUrl,			
+        user.verifiedTeacher	as user_verifiedTeacher,		
+        user.permissions	as user_permissions        
+FROM `lms_analytics.rosterLogs` as roster JOIN  user on user.id=roster.resource.userId ORDER BY roster.timestamp DESC
