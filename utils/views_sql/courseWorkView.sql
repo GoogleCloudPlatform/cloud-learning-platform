7c3290d8e7024ce24b23a7e2e8064069529ee256
shubhammenroy <shubham.menroy@quantiphi.com>
2023-03-22 14:02:01 +0530

    Added and modified BQ views

--- utils/views_sql/courseWorkView.sql
+++ utils/views_sql/courseWorkView.sql
@@ -1,6 +1,34 @@
 CREATE OR REPLACE VIEW
   courseWorkCollectionView AS
-SELECT * FROM
+SELECT 
+uuid,			
+id,			
+courseId,			
+title,			
+description,			
+materials,			
+state,			
+alternateLink,			
+creationTime,			
+updateTime,			
+dueDate,			
+dueTime,			
+scheduledTime,			
+maxPoints,			
+workType,			
+associatedWithDeveloper,			
+assigneeMode,			
+individualStudentsOptions,			
+submissionModificationMode,			
+creatorUserId,			
+topicId,			
+gradeCategory,			
+assignment,			
+multipleChoiceQuestion,			
+event_type,			
+timestamp,			
+message_id
+ FROM
 (
 SELECT *, ROW_NUMBER() OVER(PARTITION BY id ORDER BY timestamp DESC) AS row_num
 FROM `lms_analytics.courseWorkCollection`
