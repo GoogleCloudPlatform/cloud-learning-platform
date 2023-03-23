6eda8dc1ee11e2d727d279a7fe4212d6c5bd97ea
pavansareddy-q <pavan.sareddy@quantiphi.com>
2023-03-23 17:35:24 +0530

    chore: created common service for line item results

--- microservices/lti/src/routes/results.py
+++ microservices/lti/src/routes/results.py
@@ -10,6 +10,7 @@ from common.utils.http_exceptions import (InternalServerError, ResourceNotFound,
                                           Unauthenticated)
 from schemas.results_schema import (UpdateResultModel, ResultResponseModel)
 from schemas.error_schema import NotFoundErrorResponseModel
+from services.line_item_service import get_line_item_results, get_result_of_line_item
 from typing import List, Optional
 # pylint: disable=unused-argument, use-maxsplit-arg, line-too-long
 
@@ -20,80 +21,19 @@ router = APIRouter(
 
 
 @router.get(
-    "/{context_id}/results",
-    name="Get all the results for a given context",
-    response_model=List[ResultResponseModel],
-    responses={404: {
-        "model": NotFoundErrorResponseModel
-    }})
-def get_results_of_context(context_id: str,
-                           skip: int = 0,
-                           limit: int = 10,
-                           user_id: Optional[str] = None):
-  """The get all results of a line item endpoint will return all the
-  results of a line item from firestore
-  ### Args:
-  line_item_id: `str`
-    Unique identifier for line item
-  skip: `int`
-    Number of results to be skipped <br/>
-  limit: `int`
-    Size of results array to be returned <br/>
-  user_id: `str`
-    Unique identifier of the user <br/>
-  ### Raises:
-  ResourceNotFoundException:
-    If the line item does not exist. <br/>
-  Exception:
-    Internal Server Error. Raised if something went wrong
-  ### Returns:
-  Result: `ResultResponseModel`
-  """
-  try:
-    result_fields = []
-    if user_id:
-      result = Result.collection.filter("userId", "==", user_id).get()
-      if result:
-        result_fields = [{
-            "id": result.id,
-            **result.get_fields(reformat_datetime=True)
-        }]
-    else:
-      result = Result.collection.offset(skip).fetch(limit)
-      for i in result:
-        result_data = i.get_fields(reformat_datetime=True)
-        result_data[
-            "id"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{i.lineItemId}/results/{i.id}"
-        result_data[
-            "scoreOf"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{i.lineItemId}"
-        result_fields.append(result_data)
-
-    return result_fields
-
-  except InvalidTokenError as e:
-    Logger.error(e)
-    raise Unauthenticated(str(e)) from e
-  except ResourceNotFoundException as e:
-    Logger.error(e)
-    raise ResourceNotFound(str(e)) from e
-  except Exception as e:
-    Logger.error(e)
-    Logger.error(traceback.print_exc())
-    raise InternalServerError(str(e)) from e
-
-
-@router.get(
     "/{context_id}/line_items/{line_item_id}/results",
     name="Get all the results of a specific line item",
     response_model=List[ResultResponseModel],
     responses={404: {
         "model": NotFoundErrorResponseModel
     }})
-def get_results_of_line_item(context_id: str,
-                             line_item_id: str,
-                             skip: int = 0,
-                             limit: int = 10,
-                             user_id: Optional[str] = None):
+def get_results_of_line_item_for_admin(
+    context_id: str,
+    line_item_id: str,
+    skip: int = 0,
+    limit: int = 10,
+    user_id: Optional[str] = None,
+    is_grade_sync_completed: Optional[bool] = None):
   """The get all results of a line item endpoint will return all the
   results of a line item from firestore
   ### Args:
@@ -120,26 +60,13 @@ def get_results_of_line_item(context_id: str,
       raise ResourceNotFoundException(
           f"Line item with id {line_item_id} in {context_id} not found")
 
-    result_fields = []
-    if user_id:
-      result = Result.collection.filter("lineItemId", "==",
-                                        line_item_id).filter(
-                                            "userId", "==", user_id).get()
-      if result:
-        result_fields = [{
-            "id": result.id,
-            **result.get_fields(reformat_datetime=True)
-        }]
-    else:
-      result = Result.collection.filter("lineItemId", "==",
-                                        line_item_id).offset(skip).fetch(limit)
-      for i in result:
-        result_data = i.get_fields(reformat_datetime=True)
-        result_data[
-            "id"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}/results/{i.id}"
-        result_data[
-            "scoreOf"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}"
-        result_fields.append(result_data)
+    result_fields = get_line_item_results(
+        context_id=context_id,
+        line_item_id=line_item_id,
+        user_id=user_id,
+        is_grade_sync_completed=is_grade_sync_completed,
+        skip=skip,
+        limit=limit)
 
     return result_fields
 
@@ -162,7 +89,7 @@ def get_results_of_line_item(context_id: str,
     responses={404: {
         "model": NotFoundErrorResponseModel
     }})
-def get_result(context_id: str, line_item_id: str, result_id: str):
+def get_result_for_admin(context_id: str, line_item_id: str, result_id: str):
   """The get result of a line item endpoint will return the specific result
   of a line item from firestore
   ### Args:
@@ -179,22 +106,8 @@ def get_result(context_id: str, line_item_id: str, result_id: str):
   Result: `ResultResponseModel`
   """
   try:
-    line_item = LineItem.find_by_id(line_item_id)
-
-    if line_item.contextId != context_id:
-      raise ResourceNotFoundException(
-          f"Line item with id {line_item_id} in {context_id} not found")
-
-    result = Result.find_by_id(result_id)
-    if result.lineItemId != line_item_id:
-      raise ResourceNotFoundException(
-          "Incorrect result id provided for the given line item")
-    result_fields = result.get_fields(reformat_datetime=True)
-    result_fields[
-        "id"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}/results/{result.id}"
-
-    result_fields[
-        "scoreOf"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}"
+    result_fields = get_result_of_line_item(
+        context_id=context_id, line_item_id=line_item_id, result_id=result_id)
 
     return result_fields
 
--- microservices/lti/src/services/line_item_service.py
+++ microservices/lti/src/services/line_item_service.py
@@ -1,5 +1,8 @@
 """Line item service"""
-from common.models import LineItem, LTIContentItem
+from common.models import LineItem, LTIContentItem, Result
+from common.utils.errors import ResourceNotFoundException
+from config import LTI_ISSUER_DOMAIN
+# pylint: disable=line-too-long
 
 
 def create_new_line_item(input_line_item_dict):
@@ -45,3 +48,62 @@ def create_new_content_item(input_content_item_dict, context_id=None):
 
   content_item_fields["id"] = new_content_item.id
   return content_item_fields
+
+
+def get_line_item_results(context_id,
+                          line_item_id=None,
+                          user_id=None,
+                          is_grade_sync_completed=None,
+                          skip=0,
+                          limit=10):
+  """Get the results of a given line item for a specific context id"""
+  result_fields = []
+
+  result_collection_manager = Result.collection
+
+  if user_id:
+    result_collection_manager = result_collection_manager.filter(
+        "userId", "==", user_id)
+
+  if line_item_id:
+    result_collection_manager = result_collection_manager.filter(
+        "lineItemId", "==", line_item_id)
+
+  if is_grade_sync_completed is not None:
+    result_collection_manager = result_collection_manager.filter(
+        "isGradeSyncCompleted", "==", is_grade_sync_completed)
+
+  result = result_collection_manager.offset(skip).fetch(limit)
+
+  for i in result:
+    result_data = i.get_fields(reformat_datetime=True)
+    result_data[
+        "id"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}/results/{i.id}"
+    result_data[
+        "scoreOf"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}"
+    result_fields.append(result_data)
+
+  return result_fields
+
+
+def get_result_of_line_item(context_id, line_item_id, result_id):
+  """Get the result details of a given line item"""
+  line_item = LineItem.find_by_id(line_item_id)
+
+  if line_item.contextId != context_id:
+    raise ResourceNotFoundException(
+        f"Line item with id {line_item_id} in {context_id} not found")
+
+  result = Result.find_by_id(result_id)
+  if result.lineItemId != line_item_id:
+    raise ResourceNotFoundException(
+        "Incorrect result id provided for the given line item")
+
+  result_fields = result.get_fields(reformat_datetime=True)
+  result_fields[
+      "id"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}/results/{result.id}"
+
+  result_fields[
+      "scoreOf"] = f"{LTI_ISSUER_DOMAIN}/lti/api/v1/{context_id}/line_items/{line_item_id}"
+
+  return result_fields
