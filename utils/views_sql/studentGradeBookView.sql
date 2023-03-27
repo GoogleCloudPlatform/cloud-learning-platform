db82cce9f18d52d9a7850706db11b807dcf6ea2c
shubhammenroy <shubham.menroy@quantiphi.com>
2023-03-27 13:50:09 +0530

    Added grade book enriched view

--- utils/views_sql/studentGradeBookView.sql
+++ utils/views_sql/studentGradeBookView.sql
@@ -1,14 +1,15 @@
 CREATE OR REPLACE VIEW
 `lms_analytics.studentGradeBookView` as
 SELECT
-a.submittedColl_courseID AS courseWork_id,
-a.userColl_emailAddress AS emailAddress,
-a.userColl_name.fullName AS fullName,
-b.coursework_title AS coursework_title,
+a.submission_course_id AS course_id,
+a.submission_course_work_id AS courseWork_id,
+a.user_email_address AS emailAddress,
+a.user_name.fullName AS fullName,
+b.courseWork_title AS coursework_title,
 b.courseWork_maxPoints AS maxPoints,
-a.submittedColl_assignedGrade AS assignedGrade,
-a.submittedColl_draftGrade AS draftGrade,
+a.submission_assigned_grade AS assignedGrade,
+a.submission_draft_grade AS draftGrade,
 b.section_name AS section,
 b.cohort_name AS cohort
 FROM `lms_analytics.submittedCourseWorkCollectionView` a
-JOIN `lms_analytics.courseWorkCollEnrichedView` b ON a.submittedColl_courseWorkId = b.courseWork_id
+JOIN `lms_analytics.courseWorkCollEnrichedView` b ON a.submission_course_work_id = b.courseWork_id
