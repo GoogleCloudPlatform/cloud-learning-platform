ed8f187318b208c0c5b58914ef1764b8e4d1f91e
pavansareddy-q <pavan.sareddy@quantiphi.com>
2023-04-17 18:56:50 +0530

    chore: updated lti assignment schema

--- microservices/classroom_shim/src/schemas/schema_examples.py
+++ microservices/classroom_shim/src/schemas/schema_examples.py
@@ -2,42 +2,24 @@
 # pylint: disable=line-too-long
 import datetime
 
+INSERT_LTI_ASSIGNMENT_EXAMPLE = {
+    "context_id": "VFh6MzA4Jfq4bYN5",
+    "context_type": "section",
+    "lti_content_item_id": "Kv57BaY2uqE23N",
+    "lti_assignment_title": "Test Assignment",
+    "tool_id": "Z3bV9qJ7p41uCX",
+    "course_work_id": "Px5cTmh2Xvq1Cb",
+    "max_points": 100,
+    "start_date": datetime.datetime(year=2023, month=1, day=14),
+    "end_date": datetime.datetime(year=2023, month=3, day=18),
+    "due_date": datetime.datetime(year=2023, month=2, day=20)
+}
+
 LTI_ASSIGNMENT_EXAMPLE = {
     "id": "N49q6vGt29oBvn8gt",
-    "section_id": "VFh6MzA4Jfq4bYN5",
-    "lti_content_item_id": "Kv57BaY2uqE23N",
-    "lti_assignment_title": "Test Assignment",
-    "tool_id": "Z3bV9qJ7p41uCX",
-    "course_work_id": "Px5cTmh2Xvq1Cb",
-    "max_points": 100,
-    "start_date": datetime.datetime(year=2023, month=1, day=14),
-    "end_date": datetime.datetime(year=2023, month=3, day=18),
-    "due_date": datetime.datetime(year=2023, month=2, day=20)
+    **INSERT_LTI_ASSIGNMENT_EXAMPLE
 }
-
-INSERT_LTI_ASSIGNMENT_EXAMPLE = {
-    "section_id": "VFh6MzA4Jfq4bYN5",
-    "lti_content_item_id": "Kv57BaY2uqE23N",
-    "lti_assignment_title": "Test Assignment",
-    "tool_id": "Z3bV9qJ7p41uCX",
-    "course_work_id": "Px5cTmh2Xvq1Cb",
-    "max_points": 100,
-    "start_date": datetime.datetime(year=2023, month=1, day=14),
-    "end_date": datetime.datetime(year=2023, month=3, day=18),
-    "due_date": datetime.datetime(year=2023, month=2, day=20)
-}
-
-UPDATE_LTI_ASSIGNMENT_EXAMPLE = {
-    "section_id": "VFh6MzA4Jfq4bYN5",
-    "lti_content_item_id": "Kv57BaY2uqE23N",
-    "lti_assignment_title": "Test Assignment",
-    "tool_id": "Z3bV9qJ7p41uCX",
-    "course_work_id": "Px5cTmh2Xvq1Cb",
-    "max_points": 100,
-    "start_date": datetime.datetime(year=2023, month=1, day=14),
-    "end_date": datetime.datetime(year=2023, month=3, day=18),
-    "due_date": datetime.datetime(year=2023, month=2, day=20)
-}
+UPDATE_LTI_ASSIGNMENT_EXAMPLE = {**INSERT_LTI_ASSIGNMENT_EXAMPLE}
 
 LTI_POST_GRADE_MODEL = {
     "user_id": "V2bpB62b8nPb18yn9",
