02eedf5cd5a4f79af707856a71a554ea28ab0041
Shruti Math <shruti.math@quantiphi.com>
2023-04-27 08:40:36 +0530

    asu setup

--- setup/setup_local.sh
+++ setup/setup_local.sh
@@ -25,7 +25,8 @@ for variable in ${EnvVars[@]}; do
   fi
 done
 
-CLUSTER_NAME=core-learning-services-dev-us-central1
+# CLUSTER_NAME=core-learning-services-dev-us-central1
+CLUSTER_NAME=study-hall-dev-365218-us-central1
 EXPECTED_CONTEXT=gke_${PROJECT_ID}_${REGION}_${CLUSTER_NAME}
 
 BLUE=$(tput setaf 4)
@@ -52,7 +53,7 @@ init() {
   gcloud config set project $PROJECT_ID
 
   printf "\n${BLUE}Set up gcloud and kubectl context ...${NORMAL}\n"
-  gcloud container clusters get-credentials core-learning-services-dev-us-central1 --zone ${REGION} --project ${PROJECT_ID}
+  gcloud container clusters get-credentials study-hall-dev-365218-us-central1 --zone ${REGION} --project ${PROJECT_ID}
 }
 
 setup_namespace() {
