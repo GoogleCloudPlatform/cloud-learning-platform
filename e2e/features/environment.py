25c77a0116ec1a44e706f3df22f5290f647547a3
Shruti Math <shruti.math@quantiphi.com>
2023-04-28 16:27:47 +0530

    Invite student fixture modified

--- e2e/features/environment.py
+++ e2e/features/environment.py
@@ -413,15 +413,17 @@ def invite_student(context):
   course_enrollment_mapping.section = section
   course_enrollment_mapping.status ="invited"
   course_enrollment_mapping.invitation_id=invitation_dict["id"]
-  temp_user = TempUser.from_dict(TEST_USER)
+  temp_user = TempUser.find_by_email(student_data["invite_student_email"])
+  if temp_user is None:
+    temp_user = TempUser.from_dict(TEST_USER)
 
-  temp_user.user_id = ""
-  temp_user.email=student_data["invite_student_email"]
-  temp_user.gaia_id = ""
-  temp_user.photo_url = ""
-  temp_user.save()
-  temp_user.user_id = temp_user.id
-  temp_user.update()
+    temp_user.user_id = ""
+    temp_user.email=student_data["invite_student_email"]
+    temp_user.gaia_id = ""
+    temp_user.photo_url = ""
+    temp_user.save()
+    temp_user.user_id = temp_user.id
+    temp_user.update()
   course_enrollment_mapping.user = temp_user.user_id
   course_enrollment_id=course_enrollment_mapping.save().id
   context.invitation_data = {
