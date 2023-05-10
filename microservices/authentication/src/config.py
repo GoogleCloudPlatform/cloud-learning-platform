a7285dc28af70d99bf8c84f4dcaa6c5f5d08a736
dishantchawla9 <dishant.chawla@quantiphi.com>
2023-05-10 23:50:24 +0530

    chore: replaced gcp_project id with project id

--- microservices/authentication/src/config.py
+++ microservices/authentication/src/config.py
@@ -8,8 +8,8 @@ from schemas.error_schema import (UnauthorizedUserErrorResponseModel,
 
 PORT = os.environ["PORT"] if os.environ.get("PORT") is not None else 80
 
-GCP_PROJECT = os.environ.get("GCP_PROJECT", "")
-os.environ["GOOGLE_CLOUD_PROJECT"] = GCP_PROJECT
+PROJECT_ID = os.environ.get("PROJECT_ID", "")
+os.environ["GOOGLE_CLOUD_PROJECT"] = PROJECT_ID
 
 DATABASE_PREFIX = os.getenv("DATABASE_PREFIX", "")
 
