d2aff3bbbad59ce8a3d86381bf1829d766c92ddd
Shruti Math <shruti.math@quantiphi.com>
2023-05-11 15:22:43 +0530

    commit id of aitutor updated

--- common/src/common/models/temp_user.py
+++ common/src/common/models/temp_user.py
@@ -185,8 +185,7 @@ class TempUser(TempBaseModel):
   gaia_id = TextField()
   photo_url = TextField()
   is_deleted = BooleanField(default=False)
-  inspace_user = MapField(default={"user_id":"",
-                        "is_inspace_user": False})
+  inspace_user = MapField(default={})
 
   class Meta:
     collection_name = TempBaseModel.DATABASE_PREFIX + "users"
