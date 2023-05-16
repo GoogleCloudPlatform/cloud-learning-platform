eff3940b27d564c3986ad59ea4d442376434d69c
Shruti Math <shruti.math@quantiphi.com>
2023-05-16 10:24:49 +0530

    remove unused import

--- e2e/features/environment.py
+++ e2e/features/environment.py
@@ -12,8 +12,8 @@ from googleapiclient.discovery import build
 from testing_objects.test_config import API_URL_AUTHENTICATION_SERVICE,API_URL,e2e_google_form_id
 from e2e.gke_api_tests.secrets_helper import get_user_email_and_password_for_e2e,\
   get_student_email_and_token,\
-  get_required_emails_from_secret_manager,create_coursework,create_google_form,\
-get_file,get_gmail_student_email_and_token
+  get_required_emails_from_secret_manager,create_coursework\
+    ,get_gmail_student_email_and_token
 
 from testing_objects.course_template import COURSE_TEMPLATE_INPUT_DATA
 from testing_objects.user import TEST_USER
