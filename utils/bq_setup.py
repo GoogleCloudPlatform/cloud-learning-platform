bb114e0439e21cd8072082f53656922c52037e4a
shubhammenroy <shubham.menroy@quantiphi.com>
2023-05-19 19:29:20 +0530

    Added status in section view & alter table sql

--- utils/bq_setup.py
+++ utils/bq_setup.py
@@ -12,7 +12,7 @@ DATABASE_PREFIX = os.getenv("DATABASE_PREFIX", "")
 
 GCP_PROJECT = os.getenv("PROJECT_ID", None)
 
-BQ_REGION= os.getenv("BQ_REGION", "US")
+BQ_REGION = os.getenv("BQ_REGION", "US")
 
 BQ_DATASET = DATABASE_PREFIX + os.getenv("BQ_DATASET", "lms_analytics")
 
@@ -36,13 +36,14 @@ def create_table_using_sql_file(dataset, file_name):
   """Create a table using the query from the given file in the BQ dataset"""
   dataset_id = f"{GCP_PROJECT}.{dataset}"
   print("Dataset_id", dataset_id)
-  old_dataset=os.getenv("BQ_DATASET", "lms_analytics")
+  old_dataset = os.getenv("BQ_DATASET", "lms_analytics")
   job_config = bigquery.QueryJobConfig(default_dataset=dataset_id)
   with open(file_name, "r", encoding="utf-8") as file:
     query = file.read()
-  query=query.replace(old_dataset,dataset)
-  query_job = bq_client.query(
-      query, project=GCP_PROJECT, job_config=job_config)
+  query = query.replace(old_dataset, dataset)
+  query_job = bq_client.query(query,
+                              project=GCP_PROJECT,
+                              job_config=job_config)
   error = ""
   try:
     query_job.result()
@@ -75,23 +76,58 @@ def create_views(dataset):
   file_path = "views_sql/"
   sql_files = listdir(file_path)
   sql_file_list = [file_path + i for i in sql_files]
-  sql_file_list.sort(key=lambda x:x[-7:])
+  sql_file_list.sort(key=lambda x: x[-7:])
   for each_file in sql_file_list:
     print(f"Running ddl_file: {each_file}")
-    create_table_using_sql_file(dataset,
-                          each_file)
+    create_table_using_sql_file(dataset, each_file)
+
+
+def alter_table_using_sql_file(dataset, file_name):
+  """Alter a table using the query from the given file in the BQ dataset"""
+  dataset_id = f"{GCP_PROJECT}.{dataset}"
+  print("Dataset_id", dataset_id)
+  old_dataset = os.getenv("BQ_DATASET", "lms_analytics")
+  job_config = bigquery.QueryJobConfig(default_dataset=dataset_id)
+  with open(file_name, "r", encoding="utf-8") as file:
+    query = file.read()
+  query = query.replace(old_dataset, dataset)
+  query_job = bq_client.query(query,
+                              project=GCP_PROJECT,
+                              job_config=job_config)
+  error = ""
+  try:
+    query_job.result()
+    print("Completed for the file: ", file_name)
+  except BadRequest as e:
+    for e in query_job.errors:
+      error = f"ERROR: {e['message']}"
+      raise Exception(error) from e
+
+
+def alter_tables(dataset):
+  """Alter tables in the bigquery"""
+  print("dataset", dataset)
+  sql_file_list = []
+
+  file_path = "alter_tables_sql/"
+  sql_files = listdir(file_path)
+  sql_file_list = [file_path + i for i in sql_files]
+
+  for each_file in sql_file_list:
+    print(f"Running ddl_file: {each_file}")
+    alter_table_using_sql_file(dataset, each_file)
+
 
 def parse_arguments():
   """Parse the given arguments"""
   parser = argparse.ArgumentParser()
 
-  parser.add_argument(
-      "--create-dataset",
-      dest="create_dataset",
-      type=str,
-      default="false",
-      choices=["true", "false"],
-      help="Create dataset for lms_analytics? true or false")
+  parser.add_argument("--create-dataset",
+                      dest="create_dataset",
+                      type=str,
+                      default="false",
+                      choices=["true", "false"],
+                      help="Create dataset for lms_analytics? true or false")
 
   parser.add_argument(
       "--create-tables",
@@ -100,6 +136,15 @@ def parse_arguments():
       default="true",
       choices=["true", "false"],
       help="Create table for logs in lms_analytics dataset? true or false")
+
+  parser.add_argument(
+      "--alter-tables",
+      dest="alter_tables",
+      type=str,
+      default="false",
+      choices=["true", "false"],
+      help="Alter tables in lms_analytics dataset? true or false")
+
   parser.add_argument(
       "--create-views",
       dest="create_views",
@@ -120,6 +165,8 @@ if __name__ == "__main__":
   # Run the following command with the required arguments to trigger the script
   # cd utils
   # PYTHONPATH=../common/src python3 bq_setup.py
+  # For altering the tables pass these as args:
+  # --create-tables=false --create-views=false --alter-tables=true
 
   args = parse_arguments()
 
@@ -132,3 +179,5 @@ if __name__ == "__main__":
     create_tables(BQ_DATASET)
   if args.create_views == "true":
     create_views(BQ_DATASET)
+  if args.alter_tables == "true":
+    alter_tables(BQ_DATASET)
