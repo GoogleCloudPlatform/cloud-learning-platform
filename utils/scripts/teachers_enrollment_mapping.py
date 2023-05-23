0d679e0cfdbbec2046698198a2fb6baf0c7d5122
shubhammenroy <shubham.menroy@quantiphi.com>
2023-05-23 16:40:33 +0530

    fixed some scripts

--- utils/scripts/teachers_enrollment_mapping.py
+++ utils/scripts/teachers_enrollment_mapping.py
@@ -4,59 +4,56 @@
 
 # disabling for linting to pass
 # pylint: disable = broad-exception-raised, broad-except
-import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from common.models import CourseEnrollmentMapping,Section,User
+from common.models import CourseEnrollmentMapping, Section, User
 from common.utils.errors import ResourceNotFoundException
-DATABASE_PREFIX = os.getenv("DATABASE_PREFIX", "")
 
-def create_teacher_course_enrollment_mapping(teacher,section):
+
+def create_teacher_course_enrollment_mapping(teacher, section):
   """save course enrollment mapping"""
-  user=User.find_by_email(teacher.lower())
+  user = User.find_by_email(teacher.lower())
   if user:
     if not CourseEnrollmentMapping.check_enrollment_exists_section(
-      section.key,user.id):
+        section.key, user.id):
       try:
-        course_enrollment_mapping=CourseEnrollmentMapping()
-        course_enrollment_mapping.section=section
-        course_enrollment_mapping.role="faculty"
-        course_enrollment_mapping.user=User.find_by_user_id(user.user_id)
-        course_enrollment_mapping.status="active"
+        course_enrollment_mapping = CourseEnrollmentMapping()
+        course_enrollment_mapping.section = section
+        course_enrollment_mapping.role = "faculty"
+        course_enrollment_mapping.user = User.find_by_user_id(user.user_id)
+        course_enrollment_mapping.status = "active"
         course_enrollment_mapping.save()
-        print(f"Teacher {teacher} is enrrolled in {section.id}"+
+        print(f"Teacher {teacher} is enrrolled in {section.id}" +
               f" section using this {course_enrollment_mapping.id} mapping")
       except ResourceNotFoundException as re:
         print(str(re))
       except Exception as e:
         print(str(e))
     else:
-      print("Mapping already exsists for this user"+
+      print("Mapping already exsists for this user" +
             f" {teacher} in this section {section.id}")
   else:
     print(f"User not found by this {teacher} email. Section {section.id}")
 
+
 def main():
   print("Started Script")
-# Use a service account.
+  # Use a service account.
   cred = credentials.Certificate("service_creds.json")
 
   firebase_admin.initialize_app(cred)
 
   db = firestore.client()
-  collection_name=DATABASE_PREFIX + "sections"
-  enrollment_ref = db.collection(collection_name)
+  enrollment_ref = db.collection(Section.collection_name)
   docs = enrollment_ref.stream()
   for doc in docs:
-    section_dict=doc.to_dict()
+    section_dict = doc.to_dict()
     if "teachers" in section_dict.keys():
       for teacher in section_dict["teachers"]:
-        section=Section.find_by_id(doc.id)
-        create_teacher_course_enrollment_mapping(teacher,section)
+        section = Section.find_by_id(doc.id)
+        create_teacher_course_enrollment_mapping(teacher, section)
 
-if __name__=="__main__":
-  # print(CourseEnrollmentMapping.collection_name)
-  # print(Section.collection_name)
+
+if __name__ == "__main__":
   main()
-  # print(CourseEnrollmentMapping.find_by_id("oYmRtuZqwvmhwyt0herC").to_dict())
---
+++ utils/scripts/update_user_in_enrollment_mapping.py
@@ -0,0 +1,36 @@
+"""
+  Script to update user in course enrollment mapping 
+"""
+
+# disabling for linting to pass
+# pylint: disable = broad-exception-raised, broad-except
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from common.models import User, CourseEnrollmentMapping
+
+# Use a service account.
+cred = credentials.Certificate("service_creds.json")
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+enrollment_ref = db.collection(CourseEnrollmentMapping.collection_name)
+docs = enrollment_ref.stream()
+count = 0
+
+for doc in docs:
+  doc_ref = db.collection(CourseEnrollmentMapping.collection_name).document(
+      doc.id)
+  d = doc.to_dict()
+  if isinstance(d["user"], str):
+    user_ref = db.collection(User.collection_name).document(d["user"])
+    user = user_ref.get().to_dict()
+    if user:
+      doc_ref.update({"user": user_ref})
+      count = count + 1
+      print(f"{doc.id} => {doc_ref.get().to_dict()}")
+    else:
+      print(f"User not found by this user_id {d['user']}." +
+            f" Section {d['section'].id}")
+print("updation count", count)
