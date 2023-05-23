6f9489a6fd6950fd5493806cbfc3ba403a6cdc32
snquantiphi <85121166+SN-Quantiphi@users.noreply.github.com>
2023-05-23 16:24:25 +0530

    feat: SLP service migrated

--- common/src/common/utils/collection_references.py
+++ common/src/common/utils/collection_references.py
@@ -1,17 +1,38 @@
 """References for the Collection"""
 
 from common.models import (
-    LearningObject, LearningExperience, LearningResource,
-    CurriculumPathway, Achievement, User, UserGroup, Action, Permission,
-    Module, Application, Activity, Agent)
+    LearningObject, AssessmentItem, LearningExperience, LearningResource,
+    CurriculumPathway, KnowledgeServiceLearningContent, Concept, SubConcept,
+    KnowledgeServiceLearningObjective, KnowledgeServiceLearningUnit, Skill,
+    SkillServiceCompetency, Category, Domain, SubDomain, Achievement,
+    Assessment, SubmittedAssessment, User, UserGroup, Action, Permission,
+    Module, Application, Activity, Agent, Rubric,RubricCriterion)
 
 collection_references = {
+    # Assessment Service
+    "assessment_items": AssessmentItem,
+    "assessments": Assessment,
+    "submitted_assessments": SubmittedAssessment,
+    "rubric_criteria": RubricCriterion,
+    "rubrics": Rubric,
     # Learning Object Service
     "learning_resources": LearningResource,
     "learning_objects": LearningObject,
     "learning_experiences": LearningExperience,
     "curriculum_pathways": CurriculumPathway,
     "achievements": Achievement,
+    # Knowledge Service
+    "learning_resource": KnowledgeServiceLearningContent,
+    "concepts": Concept,
+    "sub_concepts": SubConcept,
+    "learning_objectives": KnowledgeServiceLearningObjective,
+    "learning_units": KnowledgeServiceLearningUnit,
+    # Skill Service
+    "skills": Skill,
+    "competencies": SkillServiceCompetency,
+    "categories": Category,
+    "domains": Domain,
+    "sub_domains": SubDomain,
     # User Management
     "user_groups": UserGroup,
     "users": User,
