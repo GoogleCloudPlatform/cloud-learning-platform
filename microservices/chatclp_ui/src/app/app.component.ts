67dc8ce68b9ddc6a13c4c8a85c712975ca6844e0
Lukman Ramsey <lramsey@google.com>
2023-05-29 10:41:41 -0400

    fixes for angular material

--- microservices/chatclp_ui/src/app/app.component.ts
+++ microservices/chatclp_ui/src/app/app.component.ts
@@ -9,7 +9,11 @@ const chatHistory = [
 ];
 
 // Mock data for models
-const models = ['Model 1', 'Model 2', 'Model 3'];
+const chatModels = ['Model 1', 'Model 2', 'Model 3'];
+
+interface ChatResponse {
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -18,18 +22,19 @@ const models = ['Model 1', 'Model 2', 'Model 3'];
 })
 export class AppComponent {
   title = 'chat-app';
-  selectedModel = models[0];
+  selectedModel = chatModels[0];
   message = '';
-  chat = chatHistory;
+  chats = chatHistory;
+	models = chatModels;
 
   constructor(private http: HttpClient) {}
 
   sendMessage() {
-    this.http.post('https://your-chat-service.com/api', {
-      model: this.selectedModel,
+    this.http.post<ChatResponse>('https://your-chat-service.com/api', {
+      llm_type: this.selectedModel,
       prompt: this.message
     }).subscribe(response => {
-      this.chat.push({ id: Date.now(), message: response['message'] });
+      this.chats.push({ id: Date.now(), message: response['content'] });
       this.message = '';
     }, error => {
       console.error(error);
--- microservices/chatclp_ui/src/app/app.module.ts
+++ microservices/chatclp_ui/src/app/app.module.ts
@@ -1,6 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,10 +20,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 
