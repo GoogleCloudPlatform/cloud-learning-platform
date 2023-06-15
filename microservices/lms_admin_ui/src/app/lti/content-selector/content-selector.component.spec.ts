1f6e18e4ef6946004c24c4ab32711b556c393c5d
Shubham Rathore <shubham.rathore@quantiphi.com>
2023-06-15 15:08:04 +0530

    content selector LTI test case

--- microservices/lms_admin_ui/src/app/lti/content-selector/content-selector.component.spec.ts
+++ microservices/lms_admin_ui/src/app/lti/content-selector/content-selector.component.spec.ts
@@ -3,14 +3,17 @@ import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack
 import { MatLegacyDialogModule, MatLegacyDialog as MatDialog, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog'
 import { ContentSelectorComponent } from './content-selector.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LtiService } from '../service/lti.service';
+import { of } from 'rxjs';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
-describe('ContentSelectorComponent', () => {
+fdescribe('ContentSelectorComponent', () => {
   let component: ContentSelectorComponent;
   let fixture: ComponentFixture<ContentSelectorComponent>;
-
+  let ltiService : LtiService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatLegacyDialogModule, HttpClientTestingModule],
+      imports: [MatLegacyDialogModule, HttpClientTestingModule, NgxSkeletonLoaderModule],
       declarations: [ ContentSelectorComponent ],
       providers : [MatSnackBar, 
         { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close', 'disableClose']) },
@@ -21,10 +24,20 @@ describe('ContentSelectorComponent', () => {
 
     fixture = TestBed.createComponent(ContentSelectorComponent);
     component = fixture.componentInstance;
+    ltiService = TestBed.inject(LtiService)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should observe getToolUrl method', ()=>{
+    const mockResponse = {success:true, url: 'https://www.test.com/testing'}
+    spyOn(ltiService, 'contentSelectionLaunch').and.returnValue(of({mockResponse}))
+    component.getToolUrl()
+    fixture.detectChanges()
+    expect(component.loadingIframe).toBeFalsy()
+    expect(ltiService.contentSelectionLaunch).toHaveBeenCalled()
+  })
 });
