f8761cecd3e26619037634ad7092fff473567d29
Lukman Ramsey <lramsey@google.com>
2023-06-22 07:39:26 -0400

    script to build QE offline

---
+++ utils/build_query_engine.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("microservices/llm_service/src")
+sys.path.append("common/src")
+
+from services.query_service import query_engine_build
+
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  
+  doc_url = args[0]
+  query_engine = args[1]
+  user_id = args[2]
+  
+  print(f"*** building query index for {doc_url}, query_engine {query_engine}, for user id {user_id}")
+  
+  params = {}
+  query_engine_build(doc_url, query_engine, user_id, params)
+  
