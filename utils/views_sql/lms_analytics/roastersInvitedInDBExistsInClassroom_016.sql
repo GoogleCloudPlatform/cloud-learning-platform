4d01e1f68d08049052e623c43f0f0c8fca616c5f
shubhammenroy <shubham.menroy@quantiphi.com>
2023-07-04 19:22:30 +0530

    Added views for roaster

---
+++ utils/views_sql/lms_analytics/roastersInvitedInDBExistsInClassroom_016.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE VIEW
+`lms_analytics.roastersInitedInDBExitsInClassroom` AS
+(WITH classroomSectionRoasterView AS (SELECT
+enrollmentView.user_email,
+enrollmentView.user_id,
+enrollmentView.enrollment_id,
+enrollmentView.enrollment_status,
+enrollmentView.section_id,
+enrollmentView.cohort_id,
+enrollmentView.invitation_id,
+enrollmentView.enrollment_role,
+roasterView.roster_collection,
+roasterView.user_gaia_id
+FROM `lms_analytics.sectionEnrollmentRecordView` enrollmentView LEFT OUTER JOIN
+`lms_analytics.courseRosterEnrichmentView` roasterView
+ON enrollmentView.section_id=roasterView.section_id
+AND roasterView.user_emailAddress=enrollmentView.user_email)
+SELECT * FROM sectionClassroomRoasterView WHERE 
+user_gaia_id IS NOT NULL and enrollment_status="invited");
