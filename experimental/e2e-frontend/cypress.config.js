8e1f576b899f26c2a4d25fc3b5ca5078ec438341
Ram Chaudhari <ram.chaudhari@quantiphi.com>
2023-08-02 17:09:33 +0530

    revert changes

--- experimental/e2e-frontend/cypress.config.js
+++ experimental/e2e-frontend/cypress.config.js
@@ -4,16 +4,10 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      return {
-        browsers: config.browsers.filter(
-          (b) => b.family === "chromium" && b.name !== "electron"
-        ),
-      };
     },
     experimentalRunAllSpecs: true,
     chromeWebSecurity: false,
     viewportHeight: 1080,
-    viewportWidth: 1920,
-    experimentalOriginDependencies: true,
+    viewportWidth: 1920
   },
 });
