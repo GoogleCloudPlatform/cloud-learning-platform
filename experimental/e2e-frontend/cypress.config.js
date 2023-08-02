36093bb907dae06f6f4cb14e1473607316f072f6
Ram Chaudhari <ram.chaudhari@quantiphi.com>
2023-08-02 17:02:36 +0530

    chore: changes in workflow for chrome

--- experimental/e2e-frontend/cypress.config.js
+++ experimental/e2e-frontend/cypress.config.js
@@ -4,11 +4,16 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return {
+        browsers: config.browsers.filter(
+          (b) => b.family === "chromium" && b.name !== "electron"
+        ),
+      };
     },
     experimentalRunAllSpecs: true,
     chromeWebSecurity: false,
     viewportHeight: 1080,
     viewportWidth: 1920,
-    experimentalOriginDependencies: true
+    experimentalOriginDependencies: true,
   },
 });
