2d1c1fbafb79f241bce61759c1910dc0666b4343
shubhammenroy <shubham.menroy@quantiphi.com>
2023-08-08 16:29:47 +0530

    added e2e and fixed unit test

---
+++ e2e/features/steps/lms/cuj07_lms_notification.py
@@ -0,0 +1,33 @@
+import datetime
+import behave
+import requests
+from e2e.test_config import API_URL
+from environment import wait
+
+
+#---------------------------lms-notifications replay---------------
+@behave.given(
+    "A user has access to the portal and wants to replay all messages")
+@wait(60)
+def step_impl_01(context):
+  context.url = f'{API_URL}/lms-notifications/replay'
+  start_datetime= datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
+  context.payload = {
+      "start_date": start_datetime.strftime("%Y-%m-%dT%H:%M:%S"),
+      "end_date": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+  }
+
+
+@behave.when("API request is send replay lms-notification using valid start-date end-date"
+             )
+def step_impl_02(context):
+
+  resp = requests.post(context.url, headers=context.header, )
+  context.status = resp.status_code
+  context.response = resp.json()
+
+
+@behave.then(
+    "Messages will be fetch from Big query and send to the Pub/Sub")
+def step_impl_03(context):
+  assert context.status == 202, "Status 202"
--- microservices/lms/src/routes/lms_notification_test.py
+++ microservices/lms/src/routes/lms_notification_test.py
@@ -4,6 +4,8 @@
 import os
 import mock
 from testing.test_config import BASE_URL
+# disabling pylint rules that conflict with pytest fixtures
+# pylint: disable=unused-argument,redefined-outer-name,unused-import
 from common.testing.client_with_emulator import client_with_emulator
 from common.testing.firestore_emulator import firestore_emulator, clean_firestore
 from schemas.schema_examples import (
@@ -18,10 +20,15 @@ mock_input = INPUT_REPLAY_NOTIFICATION_EXAMPLE.copy()
 
 
 def test_replay_notifications(client_with_emulator):
-  with mock.patch("routes.lms-notifications.Logger"):
-    with mock.patch("routes.lms-notifications.background_tasks.add_task"):
-      response = client_with_emulator.post(
-        API_URL,json=INPUT_REPLAY_NOTIFICATION_EXAMPLE)
+  with mock.patch("routes.lms_notification.Logger"):
+    with mock.patch("routes.lms_notification.publish_messages"):
+      response = client_with_emulator.post(API_URL,
+                                           json={
+                                               "start_date":
+                                               "2023-07-26T00:00:00+00:00",
+                                               "end_date":
+                                               "2023-07-28T00:00:00+00:00"
+                                           })
   assert response.status_code == 202, "Status 202"
 
 def test_replay_notifications_negative(client_with_emulator):
@@ -29,7 +36,7 @@ def test_replay_notifications_negative(client_with_emulator):
     "start_date":"2023-07-26", # incorrect, not datetime 
     "end_date":"2023-07-28T00:00:00+00:00" # correct, datetime
   }
-  with mock.patch("routes.lms-notifications.Logger"):
-    with mock.patch("routes.lms-notifications.background_tasks.add_task"):
+  with mock.patch("routes.lms_notification.Logger"):
+    with mock.patch("routes.lms_notification.publish_messages"):
       response = client_with_emulator.post(API_URL, json=negative_input)
   assert response.status_code == 422, "Status 422"
