1c8851f14b3a7ba8faffe126f9f3f6e946e38948
Ram Chaudhari <ram.chaudhari@quantiphi.com>
2023-08-17 18:18:31 +0530

    changes

--- experimental/e2e-frontend/secrets_helper.py
+++ experimental/e2e-frontend/secrets_helper.py
@@ -12,7 +12,7 @@ def get_e2e_test_user():
   user_email_password_secret_name = f"projects/{PROJECT_ID}/secrets/{user_email_password_secret_id}/versions/latest"
   user_email_password_response = client.access_secret_version(
       request={"name": user_email_password_secret_name})
-  return json.loads(user_email_password_response.payload.data.decode("UTF-8"))
+  return user_email_password_response.payload.data.decode("UTF-8")
 
 
 def get_org_test_user():
@@ -31,5 +31,17 @@ def get_org_test_user():
   }
 
 
-print(get_org_test_user())
-print(get_e2e_test_user())
+def get_data():
+  faculty_credentials = get_org_test_user()
+  e2e_test_credentials = get_e2e_test_user()
+  e2e_faculty_email = faculty_credentials.get("email")
+  e2e_faculty_password = faculty_credentials.get("password")
+
+  return {
+      "E2E_TEST_CREDS": e2e_test_credentials,
+      "E2E_FACULTY_EMAIL": e2e_faculty_email,
+      "E2E_FACULTY_PASSWORD": e2e_faculty_password,
+  }
+
+
+print(json.dumps(get_data()))
