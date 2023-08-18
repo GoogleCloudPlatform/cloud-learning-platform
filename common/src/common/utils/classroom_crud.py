24eb2bb61e26803f0c3d80fe15a17d78d7ebc1fe
Shruti Math <shruti.math@quantiphi.com>
2023-08-18 16:38:59 +0530

    lint

--- common/src/common/utils/classroom_crud.py
+++ common/src/common/utils/classroom_crud.py
@@ -8,7 +8,7 @@ from googleapiclient.errors import HttpError
 from common.utils.jwt_creds import JwtCredentials
 from common.utils.errors import InvalidTokenError, UserManagementServiceError, \
   ResourceNotFoundException,ValidationError
-from common.utils.http_exceptions import InternalServerError,ClassroomHttpException
+from common.utils.http_exceptions import InternalServerError
 from common.utils.logging_handler import Logger
 from common.utils.secrets import get_secret
 from common.models import Section
@@ -659,14 +659,6 @@ def enroll_student(headers, access_token, course_id, student_email,
     raise ValidationError(
   "Please set first name and last name in your google profile"
   ) from ke
-  # except HttpError as hte:
-  #   err= traceback.format_exc().replace("\n", " ")
-  #   Logger.error(hte)
-  #   Logger.error(err)
-  #   raise HttpError(
-  # status_code=hte.resp.status,
-  # message=str(hte)
-  # ) from hte
   create_student_in_course(access_token, student_email, course_id, course_code)
   # Check if searched user is [] ,i.e student is enrolling for first time
   # then call create user user-management API and return user data else
