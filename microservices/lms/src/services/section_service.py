5bc2ec6b40a3010c52ad20ea9262e04d806c8c3b
shrutimath2022 <97726249+shrutimath2022@users.noreply.github.com>
2023-12-14 15:49:30 +0530

    Update section_service.py

--- microservices/lms/src/services/section_service.py
+++ microservices/lms/src/services/section_service.py
@@ -1078,10 +1078,11 @@ def update_grades(material, section, coursework_id, lms_job_id, classroom_course
     # form_id = form_details["file_id"]
     # Get all responses for the form if no responses of
     # the form then return
-    form_url = material['form']['formUrl']
+    form_url = material["form"]["formUrl']
     form_l = form_url.split("/")
     form_id = form_l[-2]
-    Logger.info(f"Form is Id is {form_id}")
+    Logger.info(form_url)
+    Logger.info(f"Form  Id is {form_id}")
     all_responses_of_form = classroom_crud.\
     retrieve_all_form_responses(form_id)
 
